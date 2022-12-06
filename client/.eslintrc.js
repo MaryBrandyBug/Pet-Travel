013daa8fbf9d54776968c6b1fa2234eb7a78e476
Maria Bystrova <17mashmallow@gmail.com>
2022-12-06 14:21:55 +0300

    +rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,6 +20,12 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'react/jsx-filename-extension': 0,
-    ''
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'import/no-cycle': 0,
+    'default-param-last': 0,
+    'no-unused-expressions': 0,
+    'no-param-reassign': 0,
   },
 };
