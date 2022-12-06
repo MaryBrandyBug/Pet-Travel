2e0b48bab85fcc835d9b418e5b50f50e99ee6b25
Maria Bystrova <17mashmallow@gmail.com>
2022-12-06 17:53:46 +0300

    articles components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 import AboutUs from './components/AboutUs/AboutUs';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
 function App() {
+  const [auth, setAuth] = useState(null);
   return (
     <div className="App">
+      <Navbar auth={auth} setAuth={setAuth} />
       hi
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Articles/GreatPhotosTips/GreatPhotosTips.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function GreatPhotosTips() {
+  return (
+    <div>GreatPhotosTips</div>
+  );
+}
