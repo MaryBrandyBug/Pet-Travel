f588efc0e699f5c4764b1b4d82e6a114428370a2
Maria Bystrova <17mashmallow@gmail.com>
2022-12-06 15:59:13 +0300

    create about,footer,main,nav components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import AboutUs from './components/AboutUs/AboutUs';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
