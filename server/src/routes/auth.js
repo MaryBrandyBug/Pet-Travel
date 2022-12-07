0ea845bd5ddc6561c8f85b70f7fc22bd9282da95
Maria Bystrova <17mashmallow@gmail.com>
2022-12-07 22:13:24 +0300

    signin done

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -29,4 +29,23 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPass = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ where: { email }, raw: true });
+    if (!user) {
+      return res.status(401).json({ msg: 'Try again' });
+    }
+    const isUser = await bcrypt.compare(password, user.password);
+    if (isUser) {
+      delete user.password;
+      req.session.auth = user;
+      return res.json(user);
+    }
+  } catch (error) {
+    return res.status(400).json({ msg: error.message });
+  }
+});
+
 module.exports = router;
