67ce6bde75844c9dcd265fcd994938c92dd3e193
Andrew Kotlov <andrewkotlov48@gmail.com>
2022-12-07 21:31:42 +0300

    release navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
-import AboutUs from './components/AboutUs/AboutUs';
+// import AboutUs from './components/AboutUs/AboutUs';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import Main from './components/Main/Main';
+// import Main from './components/Main/Main';
 import BirdsPets from './components/Articles/BirdsPets/BirdsPets';
 import GreatPhotosTips from './components/Articles/GreatPhotosTips/GreatPhotosTips';
 import TipsForSeniorPets from './components/Articles/TipsForSeniorPets/TipsForSeniorPets';
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar({ auth }) {
+  return (
+
+    <div className="nav">
+      <div className="nav-header">
+        <div className="nav-title">
+          <Link to="/"><span>Pet&Travel</span></Link>
+        </div>
+      </div>
+      <div className="nav-btn">
+        <label htmlFor="nav-check">
+          <span />
+          <span />
+          <span />
+        </label>
+      </div>
+
+      <div className="nav-links">
+        <Link to="/aboutus"><span>О нас</span></Link>
+        {auth
+          ? (
+
+            <Link to="/">
+              {' '}
+              <span>{auth.name}</span>
+              {' '}
+            </Link>
+
+          )
+          : (
+            <>
+              <Link to="/signup">
+                {' '}
+                <span>Регистрация</span>
+                {' '}
+              </Link>
+              <Link to="/signin">
+                {' '}
+                <span>Вход</span>
+                {' '}
+              </Link>
+            </>
+          )}
+      </div>
+    </div>
+
+  );
+}
